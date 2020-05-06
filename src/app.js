7fa7ef563353bdfa51c58d3a2ae9cff4d262b6fe
Alejandro Sánchez <alejandro.sanchez@globant.com>
2020-05-06 15:48:45 -0500

    Fix for express server init

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const usersRouter = require('./routers/users');
 const tasksRouter = require('./routers/tasks');
 
-require('./db/mongoose');
-
 const app = express();
 
 app.use(express.json());
